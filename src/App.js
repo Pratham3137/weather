36554ae8e68ba6805bb0d29101e0826b5464957b
Prathamesh Santosh Mahadik <86146065+Pratham3137@users.noreply.github.com>
2023-11-03 23:53:01 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
+import './App.css'
 
-function App() {
+const App = () => {
+  const api = {
+    key:"a53765c75df701814120db4065624aed",
+    base:"https://api.openweathermap.org/data/2.5/"
+  }
+  const[search,setSearch]=useState("")
+  const[weather,setWeather]=useState({})
+  function searchPress(){
+    fetch(`${api.base}/weather?q=${search}&units=metric&APPID=${api.key}`)
+    .then(res=>res.json())
+    .then(data=>setWeather(data))
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Main">
+      <div className='App'>
+      <br />
+      <input type="text" onChange={(e)=>setSearch(e.target.value)}/>
+      <br /><br />
+      <button onClick={searchPress}>Search</button>
+      <br />
+      {(typeof weather.main !=="undefined")?(
+        <div>
+          <p>Name:{weather.name}</p>
+          <p>Temp:{weather.main.temp}⭐</p>
+          <p>Rain/Cloud:{weather.weather[0].main}</p>
+          <p>Description:{weather.weather[0].description}</p>
+        </div>
+      ):("Not Found")}
+      <br /><br />
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
